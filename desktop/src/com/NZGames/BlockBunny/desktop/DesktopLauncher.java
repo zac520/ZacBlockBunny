581f4760568995bc862100f33c85147f87d796a1
Zachary Knudson <zac520@aol.com>
2014-07-15 22:20:53 -0400

    Initial commit
    Empty project. Now going to try to redo the block bunny game, this time using scene2d and the Animation class that I am familiar with, as well as using Texture Packer. Good luck me.

---
+++ desktop/src/com/NZGames/BlockBunny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.NZGames.BlockBunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.NZGames.BlockBunny.BlockBunnyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlockBunnyGame(), config);
+	}
+}
---
+++ ios/src/com/NZGames/BlockBunny/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.NZGames.BlockBunny;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.NZGames.BlockBunny.BlockBunnyGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new BlockBunnyGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
